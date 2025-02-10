2523c3913cd4d5ceb4889cd5144b8f0b4e9bd6b9
gqucoldbrue <gqucoldbrue@gmail.com>
2025-02-10 01:38:15 +0000

    Pending changes exported from your codespace

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
-// App.tsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import RootNavigator from './src/navigation/RootNavigator';
+import RootNavigator from '@navigation/RootNavigator';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
