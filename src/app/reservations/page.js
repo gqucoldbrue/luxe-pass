e477dbc4d1663f48c4f25939e97503f710a3a077
STUDIO GQ <174906756+gqucoldbrue@users.noreply.github.com>
2025-07-11 22:20:51 -0400

    changed use-toast import path

--- src/app/reservations/page.js
+++ src/app/reservations/page.js
@@ -8,7 +8,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/u
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { toast } from '@/components/ui/use-toast';
+import { toast } from '@/app/components/ui/use-toast';
 import { supabase } from '@/lib/supabase';
 
 const getImageUrl = (imagePath) => {
